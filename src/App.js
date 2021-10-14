ae8ad9e1e25a3b9ce5d694d4f6e758972ef071d3
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-10-14 12:01:13 -0500

    fixed typo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Nav />
       
         <Switch>
-        <Route exact path="/">
+        <Route exact path="/react-portfolio/">
             <About />
           </Route>
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,7 @@ function About() {
                 <h3>Why Sunflowers?</h3>
 
                 <p>
-                When I look at a sunflower, it seems to say "I'm happy to be here" which is the attitude with which I aim to live my life. Sunflowers can be symbolic of many things, including but not limited to, joy, celebration, gratitude, friendship, community, and kindness. Above all, however, they represent choice. Whether I'm practicing data analytics, web development, or dancing west coast swing, I get to choose how I show up to each activity. The qualities I consisistently choose to bring to my work include curiosity, quality-work, and a commitment to continued growth and development.
+                When I look at a sunflower, it seems to say "I'm happy to be here" which is the attitude with which I aim to live my life. Sunflowers can be symbolic of many things, including but not limited to, joy, celebration, gratitude, friendship, community, and kindness. Above all, however, they represent choice. Whether I'm practicing data analytics, web development, or dancing west coast swing, I get to choose how I show up to each activity. The qualities I consistently choose to bring to my work include curiosity, quality, and a commitment to continued growth and development.
                 </p>
             
                 
